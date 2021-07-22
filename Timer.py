e65368f766e81588538f083d2feab46ee1f2a70e
Václav Pilát <pilatvacl@seznam.cz>
2021-07-22 12:37:22 +0200

    Added list check before adding a timestamp

--- Timer.py
+++ Timer.py
@@ -46,16 +46,15 @@ def load_json():
 	print_space('Loading data from "' + filename + '"...')
 	try:
 		f = open(filename, "r") # File
-		j = f.read() # JSON
-		l = json.loads(j) # List
+		timestamps = json.loads(f.read()) # List
 	except:
 		print_space('Cannot load timestamps from "' + filename + '". An empty list is used instead. ' + str(sys.exc_info()[0]), 2)
-		l = []
+		timestamps = []
 	else:
-		print_space('File "' + filename + '" contains ' + str(len(l)) + ' timestamps.', 2)
+		print_space('File "' + filename + '" contains ' + str(len(timestamps)) + ' timestamps.', 2)
 	finally:
 		f.close()
-	return l
+	return timestamps
 
 
 """
@@ -64,15 +63,16 @@ Saving data from list into json file
 def save_json(timestamps):
 	print_space('Saving data into "' + filename + '"...')
 	try:
-		f = open(filename, "w")
 		if os.path.isfile(filename): # File already exists
 			print_space('File "' + filename + '" exists. Replacing its content with new data...', 2)
+			f = open(filename, "w")
 			f.truncate()
 		else: # Creating new file
 			print_space('File "' + filename + '" doesn\'t exist. Creating new file...', 2)
-		f.write(json.dumps(timestamps))
+			f = open(filename, "w")
+		f.write(json.dumps(timestamps, indent=4, sort_keys=True))
 	except:
-		print_space('Cannot save data into "' + filename + '". ' + str(sys.exc_info()[0]))
+		print_space('Cannot save data into "' + filename + '". ' + str(sys.exc_info()[0]), 2)
 	finally:
 		f.close()
 
@@ -85,20 +85,21 @@ def get_status():
 		if os.path.isfile(filename): # File exists
 			timestamps = load_json()
 			if len(timestamps) > 0: # Printing out last timestamp
-				print_space('Last timestamp: ' + timestamps[len(timestamps) - 1]['type'] + ' from ' + timestamps[len(timestamps) - 1]['time'], 2)
+				print_space('First timestamp: #' + str(timestamps[0]['id']) + ' "' + timestamps[0]['type'] + '" from ' + timestamps[0]['time']) # Info about first timestamp
+				print_space('Last timestamp: #' + str(timestamps[len(timestamps) - 1]['id']) + ' "' + timestamps[len(timestamps) - 1]['type'] + '" from ' + timestamps[len(timestamps) - 1]['time']) # Info about last timestamp
 		else: # File doesn't exist
 			print_space('File "' + filename + '" doesn\'t exist. Creating a new timestamp will create it.', 2)
 	except:
 		print_space('An error occured. ' + str(sys.exc_info()[0]), 2)
 
+
 """
-Creates new timestamp
+Creates new timestamp of a selected type
 """
-def new_timestamp():
+def new_timestamp(type, timestamps):
 	try:
-		timestamps = load_json()
 		timestamp = {
-			'id': len(timestamps),
+			'id': len(timestamps) +1,
 			'type': 'start',
 			'time': datetime.datetime.now().strftime(dateformat)
 		}
@@ -106,6 +107,40 @@ def new_timestamp():
 		print_space('Created new timestamp: "' + str(timestamp) + '"')
 		save_json(timestamps)
 		load_json()
+	except:
+		print_space('An error occured. ' + str(sys.exc_info()[0]))
+
+
+"""
+Attempts to create a new start timestamp
+"""
+def start_timestamp():
+	try:
+		timestamps = load_json()
+		if len(timestamps) > 0:
+			if timestamps[len(timestamps) - 1]['type'] == 'start':
+				print_space('File cannot contain two same consecutive timestamps.')
+			else:
+				new_timestamp('start', timestamps)
+		else:
+			new_timestamp('start', timestamps)
+	except:
+		print_space('An error occured. ' + str(sys.exc_info()[0]))
+
+
+"""
+Attempts to create a new stop timestamp
+"""
+def stop_timestamp():
+	try:
+		timestamps = load_json()
+		if len(timestamps) > 0:
+			if timestamps[len(timestamps) - 1]['type'] == 'stop':
+				print_space('File cannot contain two same consecutive timestamps.')
+			else:
+				new_timestamp('stop', timestamps)
+		else:
+			print_space('File has to start with a "start" timestamp.')
 	except:
 		print_space('An error occured. ' + str(sys.exc_info()[0]))
 
@@ -142,15 +177,15 @@ def remove_file():
 List of all commands (with description and a pointer to a function)
 """
 commands = {
-	'help':     {'description': 'shows all usable commands',                'function': show_commands   },
-	'status':   {'description': 'shows basic information about the file',   'function': get_status      },
-	'start':    {'description': 'creates new "start" timestamp',            'function': new_timestamp   },
-	'stop':     {'description': 'creates new "stop" timestamp',             'function': new_timestamp   },
-	'days':     {'description': 'calculates time spent (day by day)',       'function': time_days       },
-	'terms':    {'description': 'calculates time spent (start to stop)',    'function': time_terms      },
-	'erase':    {'description': 'removes last timestamp',                   'function': erase_last      },
-	'delete':   {'description': 'deletes the whole file',                   'function': remove_file     },
-	'exit':     {'description': 'exits the app',                            'function': sys.exit        }
+	'help':    {'description': 'shows all usable commands',               'function': show_commands    },
+	'status':  {'description': 'shows basic information about the file',  'function': get_status       },
+	'start':   {'description': 'creates new "start" timestamp',           'function': start_timestamp  },
+	'stop':    {'description': 'creates new "stop" timestamp',            'function': stop_timestamp   },
+	'days':    {'description': 'calculates time spent (day by day)',      'function': time_days        },
+	'terms':   {'description': 'calculates time spent (start to stop)',   'function': time_terms       },
+	'erase':   {'description': 'removes last timestamp',                  'function': erase_last       },
+	'delete':  {'description': 'deletes the whole file',                  'function': remove_file      },
+	'exit':    {'description': 'exits the app',                           'function': sys.exit         }
 }
 
 
