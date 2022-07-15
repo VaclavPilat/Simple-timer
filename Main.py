6f1a6aaaa3fc5e3d6800eedb7febd788bc02c011
VaclavPilat <pilatvacl@seznam.cz>
2022-07-15 19:48:19 +0200

    Printing list of commands

--- Main.py
+++ Main.py
@@ -3,13 +3,36 @@ from core.Timer import *
 import sys
 
 
-def commands():
-    print("List of commands:")
+indent = "    "
+
+
+def prints(text: str):
+    """Prints text with an indent
+
+    Args:
+        text (str): Text to print
+    """
+    print(indent + text)
+
+
+def help():
+    """Prints out all usable commands
+    """
+    maxLength = len(max(commandList, key = len))
+    for command, description in commandList.items():
+        prints(command.ljust(maxLength) + " : " + description)
+
+
+def exit():
+    """Exits the application
+    """
+    sys.exit()
 
 
 # List of all commands (with description and a pointer to a function)
 commandList = {
-    "commands": "Prints all usable commands"
+    "help": "Prints all usable commands",
+    "exit": "Exits the application"
 }
 
 
@@ -22,7 +45,8 @@ def execute(command: str):
     if command in commandList:
         getattr(sys.modules[__name__], command)()
     else:
-        print("Command not found")
+        prints("Command not found")
+    print()
 
 
 def main(args: list):
@@ -35,14 +59,15 @@ def main(args: list):
         if len(args) > 1:
             # Executes arguments (if exist)
             for command in sys.argv[1:]:
-                print(command)
+                execute(command)
         else:
             # Asking for commands
-            execute('commands')
+            print("Usable commands:")
+            execute('help')
             while True:
                 execute(input('Enter command: '))
     except (SystemExit, KeyboardInterrupt):
-        pass
+        prints("Exitting...")
 
 
 if __name__ == '__main__':
