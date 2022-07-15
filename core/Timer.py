ceb6e84c674126143c1da63b31724e1e7c103997
VaclavPilat <pilatvacl@seznam.cz>
2022-07-15 21:59:30 +0200

    Created a Timer class

---
+++ core/Timer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+
+class Timer(object):
+    """Singleton for timer functions
+    """
+
+
+    def __new__(cls):
+        """Creating class instance
+
+        Returns:
+            Timer: Timer class instance
+        """
+        if not hasattr(cls, 'instance'):
+            cls.instance = super(Timer, cls).__new__(cls)
+        return cls.instance
